d7d07c76f95b85651f11b921904d177986e47336
riku <riku.suny@gmail.com>
2022-12-30 12:32:24 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+const theme = extendTheme({
+  colors: {
+    primary: '#5294ff',
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
